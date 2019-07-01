1f2f516e2b9f9e454d482dcc53ee803023cfc13e
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-01 17:11:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.inculde?("wow")
-    => true
-array.indlude?("Tardis")
-    => false
+array.include?("wow")
+  => true
+array.include?("Tardis")
+  => false
 end
 
 def using_sort(array)
