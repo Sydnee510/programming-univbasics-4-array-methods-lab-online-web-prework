1b8b9a137891ee016d5f0ddb2c002a6ac582593a
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-01 17:10:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.inculde?("wow")
+array.inculde?("wow")
     => true
-    array.indlude?("Tardis")
+array.indlude?("Tardis")
     => false
 end
 
